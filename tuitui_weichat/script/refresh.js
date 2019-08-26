06eebad5927f53c437394fad1145d13c6217fccd
efdssdf <efdssdf@gmail.com>
2019-08-26 16:40:51 +0800

    'fix'

--- tuitui_weichat/script/refresh.js
+++ tuitui_weichat/script/refresh.js
@@ -34,8 +34,7 @@ var refreshComponentAccessToken = async function() {
 
 //refresh pre_auth_code every 20 minutes
 var refreshComponentAuthCode = async function() {
-    var access_token = await mem.get("component_access_token");
-    console.log(access_token,'------------------------acc')
+    var access_token = await mem.get("cms_component_access_token");
     if(!access_token) {
         return;
     }
