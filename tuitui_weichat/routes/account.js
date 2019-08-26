02f184626002c3f47f23d09b8c3c78707af764a8
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-08-26 18:00:23 +0800

    登录，修改密码

--- tuitui_weichat/routes/account.js
+++ tuitui_weichat/routes/account.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const AccountModel = require('../model/Account.js')
 
 router.post('/', async (req, res, next) => {
-  let { account, password, belongTo } = req.body;
+  let { account, password } = req.body;
   let result = await AccountModel.find({account});
   if(result.length > 0) {
     res.send({code: 2, msg: "该账户名已存在，请检查输入是否有误"})
   } else {
-    let data = await AccountModel.create({ account, password, belongTo });
+    let data = await AccountModel.create({ account, password });
     if(data) {
       res.send({code: 1, msg: '账户创建成功', data})
     } else {
@@ -31,6 +31,16 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.put('/', async (req, res, next) => {
+  let { id, password } = req.body;
+  let data = await AccountModel.findByIdAndUpdate(id, { password }, {new: true});
+  if(data) {
+    res.send({code: 1, msg: '密码修改成功', data})
+  } else {
+    res.send({code: -1, msg: '密码修改失败，请重试'})
+  }
+});
+
 router.delete('/', async (req, res, next) => {
   let { _id } = req.query;
   let result = await AccountModel.findByIdAndRemove(_id);
@@ -38,6 +48,16 @@ router.delete('/', async (req, res, next) => {
     res.send({code: 1, msg: '删除成功'})
   } else {
     res.send({code: -1, msg: "删除失败"})
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  let { account, password } = req.body;
+  let result = await AccountModel.find({account, password});
+  if(result.length > 0) {
+    res.send({code: 1, msg: '登录成功', data: result})
+  } else {
+    res.send({code: -1, msg: '用户名或密码输入有误，请重新输入'})
   }
 });
 
