4b6f666a363e84b4c9a796bda7165bc1d9cf10c2
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-08-26 19:48:28 +0800

    ok

--- tuitui_weichat/routes/index.js
+++ tuitui_weichat/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/admin/*', function(req, res, next) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+});
 
 router.get('/admin', function(req, res, next) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+});
 
 
 
