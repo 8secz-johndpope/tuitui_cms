dcb59223f0dfd3b2e2140aae3c7e87a601852793
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-08-27 11:39:03 +0800

    ok

--- tuitui_weichat/model/TuiguangTag.js
+++ tuitui_weichat/model/TuiguangTag.js
@@ -4,7 +4,8 @@ var connect_url = require('../conf/proj.json').mongodb;
 var db = mongoose.createConnection(connect_url);
 
 var TuiguangTagSchema = new Schema({
-  name:{ type: String, required: true }
+  name:{ type: String, required: true },
+  account_id: String
 });
 
 var TuiguangTagModel = db.model('TuiguangTag', TuiguangTagSchema);
