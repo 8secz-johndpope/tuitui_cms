27913e3915fef825769edc9a63de1720964b97e7
efdssdf <efdssdf@gmail.com>
2019-08-28 10:47:35 +0800

    'fix'

--- tuitui_weichat/script/test.js
+++ tuitui_weichat/script/test.js
@@ -2,9 +2,11 @@ var mem = require('../util/mem.js');
 var WechatUtil = require('../util/get_weichat_client.js');
 
 async function a() {
-    let client = await WechatUtil.getClient(10000000001)
-    client.getFollowers(async function (err, result) {
-        console.log(err,result,'----------------------result')
-    })
+    var access_token = await mem.get("cms_component_access_token");
+    console.log(access_token,'------------------------------access_token')
+    // let client = await WechatUtil.getClient(10000000001)
+    // client.getFollowers(async function (err, result) {
+    //     console.log(err,result,'----------------------result')
+    // })
 }
 a()
