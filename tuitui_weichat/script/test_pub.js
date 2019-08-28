6095057a7f95e07b6e476ac92e9b9be4ffc771ed
efdssdf <efdssdf@gmail.com>
2019-08-28 14:54:35 +0800

    'fix'

--- tuitui_weichat/script/test_pub.js
+++
@@ -1,8 +0,0 @@
-const asyncRedis = require("async-redis");
-const redis_client = asyncRedis.createClient();
-
-async function pubAccessToken(){
-	await redis_client.publish('access_token','xxxxxxxxxx');
-}
-
-pubAccessToken()
