2215a120d28d2add17f14c75b0af84de484c46a6
efdssdf <efdssdf@gmail.com>
2019-08-30 15:00:25 +0800

    'fix'

--- tuitui_weichat/routes/component.js
+++ tuitui_weichat/routes/component.js
@@ -295,7 +295,7 @@ async function reply(req, res, message, code, type, param, openid, sex) {
 
 async function replyMsg(req, res, message, content, code, openid) {
     if (content.type == 0) {
-        res.send(wxReplay.get_reply(req, content.description, message))
+        res.send(wxReplay.get_reply(req, content.contents[0].description, message))
     } else if (content.type == 1) {
         res.send(wxReplay.get_reply(req, content.contents, message))
     }
