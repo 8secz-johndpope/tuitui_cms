583e0a812b2b08a01d425299144d60a579092542
DESKTOP-1C3BJVH\明星说-李鑫 <weblixin@163.com>
2019-08-30 11:09:40 +0800

    ok

---
+++ tuitui_weichat/build/precache-manifest.e988699c6e5ea08efbc7002c9095fb18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c8c4b89e2e4a7b810583890ed5a53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e382022418d4ec1424f7",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "084a82350750ca4d24d2",
+    "url": "/static/css/main.d7bd86f0.chunk.css"
+  },
+  {
+    "revision": "e382022418d4ec1424f7",
+    "url": "/static/js/2.0fe9aa40.chunk.js"
+  },
+  {
+    "revision": "084a82350750ca4d24d2",
+    "url": "/static/js/main.45004717.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e709b51a6a8f8bf2561d3a85c66267c3",
+    "url": "/static/media/bg.e709b51a.png"
+  },
+  {
+    "revision": "e33471629415c521f7ab25608d016801",
+    "url": "/static/media/login_bg.e3347162.png"
+  }
+]);
