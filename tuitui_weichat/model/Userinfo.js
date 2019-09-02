2eda3e57ad1242cfd191b97db37d1aace8f6855a
efdssdf <efdssdf@gmail.com>
2019-09-02 17:23:57 +0800

    'fix'

--- tuitui_weichat/model/Userinfo.js
+++ tuitui_weichat/model/Userinfo.js
@@ -16,6 +16,32 @@ var UserinfoSchema = new Schema({
     action_time: Number,
 });
 
+UserinfoSchema.statics = {
+    fetch(id, sex, tagId, codes, cb) {
+        let sql = {
+            subscribe_flag: true,
+            //$or: [{send_time: {$lt: Date.now() - 2 * 3600 * 1000}}, {send_time: 0},{send_time:{$exists:false}}],
+            code: {$in: codes},
+            action_time: {$gt: Date.now() - 48 * 3600 * 1000}
+        }
+        if (sex && sex != 'all') {
+            sql.sex = sex
+        }
+        if (tagId) {
+            sql.tagIds = {$elemMatch: {$eq: tagId}}
+        }
+        if (id) {
+            sql._id = {$lt: id}
+        }
+
+        return this.find(sql)
+            .limit(50)
+            .sort({'_id': -1})
+            .exec(cb);
+    }
+}
+
+
 var UserinfoModel = db.model('Userinfo', UserinfoSchema);
 
 module.exports = UserinfoModel;
