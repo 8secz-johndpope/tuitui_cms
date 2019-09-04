7143bc8712b58bff57e734cdcccb1e644c1a8f7f
Administrator <lionel@buzzy.cc>
2019-09-04 13:45:52 +0800

    1

--- tuitui_weichat/script/uploadMaterial.js
+++ tuitui_weichat/script/uploadMaterial.js
@@ -10,6 +10,19 @@ async function uploadNews(code, messages) {
       //console.log(item.thumb_url)
       let path = await handleImage(item.thumb_url)
       item.thumb_media_id = await uploadImage(path, code);
+      
+      return {
+        "title": item.title,
+        "thumb_media_id": item.thumb_media_id,
+        "author": item.author,
+        "digest": item.digest,
+        "show_cover_pic": item.show_cover_pic,
+        "content": item.content,
+        "content_source_url": item.content_source_url,
+        "need_open_comment": item.need_open_comment,
+        "only_fans_can_comment": item.only_fans_can_comment
+      } 
+
       return {
         "title": encodeURIComponent(item.title),
         "thumb_media_id": item.thumb_media_id,
