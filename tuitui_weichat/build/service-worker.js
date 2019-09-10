4a587fce77f90034d6b848f3fc76aca655d05732
weblixin <weblixin@163.com>
2019-09-10 19:18:20 +0800

    ok

--- tuitui_weichat/build/service-worker.js
+++ tuitui_weichat/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af251243a3f4c01ad68b505f3e012863.js"
+  "/precache-manifest.ee382cf7d3be97b69c69158d95018db3.js"
 );
 
 self.addEventListener('message', (event) => {
