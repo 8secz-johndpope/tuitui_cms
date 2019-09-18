4a1c7f0d36be49e21e25ce9e0d30e9b709fc8777
efdssdf <efdssdf@gmail.com>
2019-09-18 14:20:31 +0800

    'fix'

--- tuitui_weichat/routes/template.js
+++ tuitui_weichat/routes/template.js
@@ -10,10 +10,10 @@ router.get('/list', async(req, res, next) => {
     api.getAllPrivateTemplate(async function (err, lists) {
         for (let list of lists.template_list) {
             let body = ''
-            let reg = /\n\W.*\{/g
+            let reg = /\n\W.*\}/g
             if (reg.test(list.content)) {
-                body = list.content.match(/\n\W.*\{/g).toString()
-                body = body.replace(/\n/g, '').replace(/{/g, '').replace(/}/g, '').replace(/ /g, '').replace(/：/g, '')
+                body = list.content.match(/\n\W.*\}/g).toString()
+                body = body.replace(/\n/g, '').replace(/{/g, '').replace(/}/g, '').replace(/ /g, '').replace(/：/g, '_').replace(/.DATA/g, '')
             }
             await mem.set(code + '_' + list.template_id, body, 24 * 60 * 60)
         }
@@ -28,42 +28,40 @@ router.post('/send', async(req, res, next) => {
     let url = req.body.url
     let content = req.body.content
     let client = await wechat_util.getClient(code);
-    console.log(client, '-----------------------------client')
     send_template('', code, client, templateId, url, content)
     let body = await mem.get(code + '_' + templateId)
-    console.log(body, '-----------------------------body')
     body = body.split(',')
-    let obj = {"开始": content.first || ""}
+    let obj = {"开始": content.first.value || ""}
     for (let i = 0; i < body.length - 1; i++) {
-        obj[body[i]] = content['keynote' + (i + 1)]
+        let key = body[i].split('_')[0]
+        let value = body[i].split('_')[1]
+        obj[key] = content[value].value
     }
-    obj['结束'] = content.remark || ""
-    console.log(obj, '---------------------obj')
+    obj['结束'] = content.remark.value || ""
     await TemplateModel.create({code: code, templateId: templateId, url: url, content: obj})
     res.send('已发送')
 })
 
 async function send_template(openid, code, client, templateId, url, data) {
-    console.log(data,'-------------------------data')
-    // client.getFollowers(openid, async function (err, result) {
-    //     if (err) {
-    //         console.log(err, '------------------send template err')
-    //     } else {
-    //         if (result.errcode) {
-    //             console.log(result.errcode, '------------------send template error')
-    //         }
-    //         if (result && result.data && result.data.openid) {
-    //             for (let sendopenid of result.data.openid) {
-    //                 client.sendTemplate(sendopenid, templateId, url, data, function (err, result) {
-    //                 })
-    //             }
-    //             send_template(result.next_openid, code, client, templateId, url, data)
-    //         } else {
-    //             console.log(code + '-------------send template end')
-    //             return
-    //         }
-    //     }
-    // })
+    client.getFollowers(openid, async function (err, result) {
+        if (err) {
+            console.log(err, '------------------send template err')
+        } else {
+            if (result.errcode) {
+                console.log(result.errcode, '------------------send template error')
+            }
+            if (result && result.data && result.data.openid) {
+                for (let sendopenid of result.data.openid) {
+                    client.sendTemplate(sendopenid, templateId, url, data, function (err, result) {
+                    })
+                }
+                send_template(result.next_openid, code, client, templateId, url, data)
+            } else {
+                console.log(code + '-------------send template end')
+                return
+            }
+        }
+    })
 }
 
 module.exports = router;
