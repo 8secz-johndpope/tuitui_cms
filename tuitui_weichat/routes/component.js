6024e9e6d244426e2f4355bedb2719c066f02bb8
efdssdf <efdssdf@gmail.com>
2019-09-23 11:40:46 +0800

    'test'

--- tuitui_weichat/routes/component.js
+++ tuitui_weichat/routes/component.js
@@ -160,14 +160,14 @@ router.post('/message/:appid/callback', xml_msg, async(req, res, next) => {
     let appid = req.params.appid;
     let code
     if (appid) {
-        code = await mem.get("configure_appid_" + appid)
+        // code = await mem.get("configure_appid_" + appid)
         if (!code) {
             let conf = await ConfigModel.findOne({appid: appid})
             if (!conf) {
                 return res.send('')
             }
             code = conf.code
-            await mem.set("configure_appid_" + appid, code, 30 * 24 * 3600)
+            // await mem.set("configure_appid_" + appid, code, 30 * 24 * 3600)
         }
     }
 
