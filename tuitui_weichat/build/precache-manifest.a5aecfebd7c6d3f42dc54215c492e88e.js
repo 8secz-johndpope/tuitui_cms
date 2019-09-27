736a5fb00db7638a7bbd5ec97f93944cf0df1600
weblixin <weblixin@163.com>
2019-09-27 10:37:40 +0800

    自动回复

---
+++ tuitui_weichat/build/precache-manifest.a5aecfebd7c6d3f42dc54215c492e88e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272542e65cab5cb3a0e28996dafb4540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5be87d818d2f59c2223",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "7b9c16ecf386178d0bf3",
+    "url": "/static/css/main.aafd0281.chunk.css"
+  },
+  {
+    "revision": "b5be87d818d2f59c2223",
+    "url": "/static/js/2.03ad42e5.chunk.js"
+  },
+  {
+    "revision": "7b9c16ecf386178d0bf3",
+    "url": "/static/js/main.a56ae2f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e33471629415c521f7ab25608d016801",
+    "url": "/static/media/login_bg.e3347162.png"
+  },
+  {
+    "revision": "ad641e9fcd5ab190f20553f57dafd5b0",
+    "url": "/static/media/logo.ad641e9f.png"
+  }
+]);
--- tuitui_weichat/build/service-worker.js
+++ tuitui_weichat/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d56797ac046299539bfe0e8b35393147.js"
+  "/precache-manifest.a5aecfebd7c6d3f42dc54215c492e88e.js"
 );
 
 self.addEventListener('message', (event) => {
