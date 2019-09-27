32db286dd10a15ecaa871bf3eb9405c9d394a91e
weblixin <weblixin@163.com>
2019-09-27 18:55:01 +0800

    ok

--- tuitui_weichat/model/Message.js
+++ tuitui_weichat/model/Message.js
@@ -11,10 +11,7 @@ var MessageSchema = new Schema({
     action_type: {type: Number, default: 0},
     img: String,
     url: String,
-    isHour: {
-        type: Number,
-        default: 0
-    }, // 0 分钟 1 小时
+
     title: String,
     delay: Number,
     des: String,
