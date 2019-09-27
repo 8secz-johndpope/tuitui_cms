42a5caad31f1ff26146d710567c0da660bd4c1db
weblixin <weblixin@163.com>
2019-09-27 11:49:31 +0800

    自动回复消息

--- tuitui_weichat/routes/component.js
+++ tuitui_weichat/routes/component.js
@@ -287,7 +287,7 @@ async function reply(req, res, message, code, type, param, openid, sex) {
 
         if (type == 0) {
             console.log(new Date(), "lixin0", param)
-            let aa = await ReplyModel.find({account_id: req.session.account._id})
+            let aa = await ReplyModel.find({code: code})
 console.log(aa, "------------------lixin-----------------aa------------------")
             reply = await ReplyModel.findOne({
                 $or: [
