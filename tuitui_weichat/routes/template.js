bf43434dc80755b52294a69770d130f05d8e0ca4
weblixin <weblixin@163.com>
2019-09-29 15:27:11 +0800

    ok

--- tuitui_weichat/routes/template.js
+++ tuitui_weichat/routes/template.js
@@ -159,7 +159,7 @@ router.post('/preview', async(req, res, next) => {
     let client = await wechat_util.getClient(code);
     let result = await preview_template(openid, code, client, templateId, url, content);
     if(result) {
-        res.send({codeL 1, msg: "'已发送预览消息'"})
+        res.send({code: 1, msg: "'已发送预览消息'"})
     }
 });
 
