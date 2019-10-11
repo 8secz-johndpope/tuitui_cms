280ae06536f825d933e8481481fd50ae49bf19c5
weblixin <weblixin@163.com>
2019-10-11 11:14:20 +0800

    ok

--- tuitui_weichat/routes/component.js
+++ tuitui_weichat/routes/component.js
@@ -280,7 +280,7 @@ async function reply(req, res, message, code, type, param, openid, sex) {
             }).sort({type: 1})
         } else if (type == 1) {
             reply = await MenuModel.find({codes: {$elemMatch: {$eq: code}}}).sort({updateAt: -1}).limit(1);
-            reply = reply[0][param] !== "undefined" ? reply[0][param] : {}
+            reply = !!reply[0][param] ? reply[0][param] : {}
             // reply = await ReplyModel.findOne({codes: {$elemMatch: {$eq: code}}, type: type, key: param})
         } else if (type == 2) {
             reply = await ReplyModel.findOne({
