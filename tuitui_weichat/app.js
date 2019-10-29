10048319c6c35c894d7aebe76ef0b0057efd1fa1
weblixin <weblixin@163.com>
2019-10-29 16:00:36 +0800

    ok

--- tuitui_weichat/app.js
+++ tuitui_weichat/app.js
@@ -124,6 +124,11 @@ app.use('/tuiguangTag',[cookieParser(),sessiond],tuiguangTag)
 app.use('/template',[cookieParser(),sessiond],template)
 app.use('/messageGroup',[cookieParser(),sessiond],messageGroup)
 
+app.use(function(req, res, next) {
+  console.log(req.session, "===============app.js===========================");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
