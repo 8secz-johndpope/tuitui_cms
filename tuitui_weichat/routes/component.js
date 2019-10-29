6c7c0a870ee77b6828998a750f19e227e293910a
Administrator <lionel@buzzy.cc>
2019-10-29 19:07:35 +0800

    1

--- tuitui_weichat/routes/component.js
+++ tuitui_weichat/routes/component.js
@@ -223,9 +223,6 @@ router.get('/unbind', async(req, res, next) => {
 router.post('/message/:appid/callback', xml_msg, async(req, res, next) => {
 
     let appid = req.params.appid;
-    /*if (appid != 'wx3805806832e4f552' && appid != 'wx0b2522b49584c154' && appid != 'wx4653895b5676edeb') {
-        return res.send('');
-    }*/
     let code
     if (appid) {
         code = await mem.get("configure_appid_" + appid)
@@ -250,6 +247,10 @@ router.post('/message/:appid/callback', xml_msg, async(req, res, next) => {
     } else if (message.Content == 'openid') {
         console.log('---回复openid-----')
         return res.send(wxReplay.get_reply(req, message.FromUserName, message))
+    }
+
+    if (appid != 'wx3805806832e4f552' && appid != 'wx0b2522b49584c154' && appid != 'wx4653895b5676edeb') {
+        return res.send('');
     }
 
     let user = {openid: message.FromUserName, code: code, action_time: Date.now()}
