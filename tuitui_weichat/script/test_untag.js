0cd00ad5fc8c650b8c517abe1e4cf9be11f045ad
efdssdf <efdssdf@gmail.com>
2019-12-04 15:35:00 +0800

    'script'

--- tuitui_weichat/script/test_untag.js
+++ tuitui_weichat/script/test_untag.js
@@ -1,10 +1,10 @@
 var wechat_util = require('../util/get_weichat_client.js')
 var UserTagModel = require('../model/UserTag')
-var SubOpenidTagModel = require('../model/SubOpenidTag');
+var SubOpenidSexModel = require('../model/SubOpenidSex');
 var ConfigModel = require('../model/Config');
 
 function update_tag(_id, code, tagId, sex) {
-    SubOpenidTagModel.fetchTag(_id, code, sex, async function (error, users) {
+    SubOpenidSexModel.fetchTag(_id, code, sex, async function (error, users) {
         var user_arr = [];
         users.forEach(function (user) {
             user_arr.push(user.openid)
@@ -16,7 +16,7 @@ function update_tag(_id, code, tagId, sex) {
         } else {
             client.membersBatchtagging(tagId, user_arr, async function (error, res) {
                 console.log(res)
-                if (res.errcode == 45009) {
+                if(res.errcode == 45009){
                     let conf = await ConfigModel.findOne({code: code})
                     let appid = conf.appid
                     client.clearQuota(appid, function (err, data) {
@@ -24,11 +24,11 @@ function update_tag(_id, code, tagId, sex) {
                         console.log('clearQuota end')
                         update_tag(users[0]._id, code, tagId, sex)
                     })
-                } else {
+                }else{
                     if (users.length == 50) {
-                        setTimeout(function () {
+                        setTimeout(function() {
                             update_tag(users[49]._id, code, tagId, sex)
-                        }, 200)
+                        },200)
                     } else {
                         console.log('.........end...........')
                         return
@@ -41,25 +41,15 @@ function update_tag(_id, code, tagId, sex) {
 
 async function getTag() {
     let code = process.argv.slice(2)[0]
-    let config = await ConfigModel.findOne({code: code})
-    await UserTagModel.find({code: code}, async function (err, data) {
+    UserTagModel.find({code: code}, function (err, data) {
         for (let i of data) {
-            let sex, tag, id = i.id
+            let sex = "0"
             if (i.name == "男") {
                 sex = "1"
             } else if (i.name == "女") {
                 sex = "2"
-            } else {
-                sex = "0"
-                if (config.attribute == 1) {
-                    tag = await UserTagModel.findOne({code: code, sex: '1'})
-                    id = tag.id
-                } else if (config.attribute == 2) {
-                    tag = await UserTagModel.findOne({code: code, sex: '2'})
-                    id = tag.id
-                }
             }
-            update_tag(null, code, id, sex)
+            update_tag(null, code, i.id, sex)
         }
     })
 }
