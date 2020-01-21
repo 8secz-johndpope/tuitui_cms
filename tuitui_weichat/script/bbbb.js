67fe6ee98ac1c8069a1b259c7f8e2a6c92b508e8
zhangzicong6 <zhangzicong6@gmail.com>
2020-01-22 01:42:13 +0800

    1

--- tuitui_weichat/script/bbbb.js
+++ tuitui_weichat/script/bbbb.js
@@ -3,6 +3,6 @@ const mem = require('../util/mem.js');
 async function b() {
     let code = process.argv.slice(2)[0]
     let token = await mem.get('access_token_' + code)
-    cosole.log(token)
+    console.log(token)
 }
 b()
