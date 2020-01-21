67de47e39ac21076d2ab63f97938a70cb70c9223
zhangzicong6 <zhangzicong6@gmail.com>
2020-01-22 01:40:35 +0800

    1

--- tuitui_weichat/script/bbbb.js
+++ tuitui_weichat/script/bbbb.js
@@ -1,10 +1,8 @@
-var Memcached = require('memcached');
-var memcached = new Memcached('127.0.0.1:11211');
+const mem = require('../util/mem.js');
 
 async function b() {
     let code = process.argv.slice(2)[0]
-    memcached.get('access_token' + code, function (err, token) {
-        console.log(token, '------------test')
-    })
+    let token = await mem.get('access_token_' + code)
+    cosole.log(token)
 }
 b()
