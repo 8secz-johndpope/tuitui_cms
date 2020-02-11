0f9c183740137d4555c57ad7a45c54bdbedb1f80
weblixin <weblixin@163.com>
2020-02-11 22:44:42 +0800

    数据统计

--- tuitui_weichat/build/service-worker.js
+++ tuitui_weichat/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b810b69fffc4cc283aecd091da541528.js"
+  "/precache-manifest.24d28595533f5a0bfa5eec3ae005c809.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
