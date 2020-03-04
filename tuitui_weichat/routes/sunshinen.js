81b76adcd3629d0056acdb3673369aec7a105aa9
zhangzicong6 <zhangzicong6@gmail.com>
2020-03-04 22:17:31 +0800

    1

--- tuitui_weichat/routes/sunshinen.js
+++ tuitui_weichat/routes/sunshinen.js
@@ -1,24 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const SunshineModel = require('../model/Sunshine.js');
-var mem = require('../util/mem.js')
+const mem = require('../util/mem.js')
+const rp = require('request-promise');
 
 router.get('/:id',async function (req, res, next) {
     var id = req.params.id;
     let value = await mem.get('sun_' + id)
     if(value){
         value = JSON.parse(value)
-        res.redirect(get_link(value,req))
+        res.redirect(value.tuiguang_link)
     }else {
         let data = await SunshineModel.findOne({_id: id})
         if (data) {
             await mem.set('sun_' + id,JSON.stringify(data),60)
-            res.redirect(get_link(data,req))
+            res.redirect(data.tuiguang_link)
         }else{
             res.send('没有查询到此链接，请先创建')
         }
     }
+    upload_sun()
 })
+
+
+let upload_sun =async (req) =>{
+    let s_url = 'http://www.dev.kpread.com/api/toutiao/visitorsave4join?'
+    s_url += "ip="+req.clientIp+"&";
+    s_url += "ua="+req.headers['user-agent']+"&"
+    s_url += "url="+encodeURIComponent("https://t.1yuedu.cn"+req.originalUrl)
+    let body = await rp(url)
+    console.log('------回传阳光-------',body)
+}
+
 
 let get_link = (data,req) =>{
     /*console.log('----------阳光--------')
